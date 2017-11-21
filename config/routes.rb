ad965db657cf4d445049bebc4ebb2797e89e9579
pusha <dvpusha@gmail.com>
2017-11-21 05:48:35 +0000

    added routes for API requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  scope :ISteamItems_730 do
+    get '/CurrentPrice/v1/:market_hash_name', to: 'items730/current_price#v1'
+    get '/AveragePrice/v1/:market_hash_name', to: 'items730/average_price#v1'
+    get '/GetItemsList/v1/', to: 'items730/get_items_list#v1' #пагинацию тут надо сделать и параметр per_page?
+  end
 end
