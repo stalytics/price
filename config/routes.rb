322d47c9eb0f725321c6e3b41b3ddb83ad0a8b01
pusha <dvpusha@gmail.com>
2017-11-21 05:54:43 +0000

    small fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   scope :ISteamItems_730 do
     get '/CurrentPrice/v1/:market_hash_name', to: 'items730/current_price#v1'
     get '/AveragePrice/v1/:market_hash_name', to: 'items730/average_price#v1'
-    get '/GetItemsList/v1/', to: 'items730/get_items_list#v1' #пагинацию тут надо сделать и параметр per_page?
+    get '/GetItemsList/v1/', to: 'items730/get_items_list#v1'
   end
 end
